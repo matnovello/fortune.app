5ff914d7647cd6d88c4658e226ddc25670c48948
Mat <mvello66@gmail.com>
2021-02-21 12:42:39 -0500

    gets fortune.json.jb render working in controller, adds fortune instance variables

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,4 +1,10 @@
 class Api::PagesController < ApplicationController
   def fortune_action
+    @fortune1 = "You will inherit a lot of ice cream"
+    @fortune2 = "You won't get a fortune at all"
+    @fortune3 = "You will almost fall down a stiar tomorrow, but catch yourself just in time"
+    fortunes = [@fortune1, @fortune2, @fortune3]
+
+    render "fortune.json.jb"
   end
 end
