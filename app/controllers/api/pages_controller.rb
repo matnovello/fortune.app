e91b9b54a0072709e41d2553342d50818f4cac64
Mat <mvello66@gmail.com>
2021-02-21 13:10:02 -0500

    revises fortune and random

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,12 +1,8 @@
 class Api::PagesController < ApplicationController
   def fortune_action
-    @fortune1 = "You will inherit a lot of ice cream"
-    @fortune2 = "You won't get a fortune at all"
-    @fortune3 = "You will almost fall down a stair tomorrow, but catch yourself just in time"
-    #fortunes is now an array of indexes...
-    @fortunes = [@fortune1, @fortune2, @fortune3]
+    @fortune = ["You will inherit a lot of ice, cream", "You won't get a fortune at all", "You will almost fall down a stair tomorrow, but catch yourself just in time"]
     #to randomize, create new variable!
-    @random = @fortunes.sample
+    @random = @fortune.sample
 
     render "fortune.json.jb"
   end
