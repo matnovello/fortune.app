1b9590a964ae702c8c3dce5c3141261cc07e9f9c
Mat <mvello66@gmail.com>
2021-02-21 13:05:35 -0500

    fortunes random method

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,8 +2,11 @@ class Api::PagesController < ApplicationController
   def fortune_action
     @fortune1 = "You will inherit a lot of ice cream"
     @fortune2 = "You won't get a fortune at all"
-    @fortune3 = "You will almost fall down a stiar tomorrow, but catch yourself just in time"
-    fortunes = [@fortune1, @fortune2, @fortune3]
+    @fortune3 = "You will almost fall down a stair tomorrow, but catch yourself just in time"
+    #fortunes is now an array of indexes...
+    @fortunes = [@fortune1, @fortune2, @fortune3]
+    #to randomize, create new variable!
+    @random = @fortunes.sample
 
     render "fortune.json.jb"
   end
